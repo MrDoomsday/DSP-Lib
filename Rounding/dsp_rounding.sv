4305a526a218439dd6e252d87c8f5da907a8e53b
RB <belenkov_rn@kursksu.ru>
2025-08-24 22:55:19 +0300

    dev(rounding): добавлен режим округления HALF_DOWN

--- Rounding/dsp_rounding.sv
+++ Rounding/dsp_rounding.sv
@@ -1,7 +1,7 @@
 module dsp_rounding #(
     parameter int unsigned IWIDTH = 32,
     parameter int unsigned OWIDTH = 32,
-    parameter string ROUND_TYPE = "CONVERGENT"
+    parameter string ROUND_TYPE = "CONVERGENT" // TRUNCATION, HALF_UP, HALF_DOWN, TO_ZERO, FROM_ZERO, CONVERGENT
 ) (
     input logic [IWIDTH-1:0] in,
     output logic [OWIDTH-1:0] out
@@ -14,6 +14,9 @@ module dsp_rounding #(
         end else if(ROUND_TYPE == "HALF_UP") begin: half_up
             wire [IWIDTH-1:0] hu = in[IWIDTH-1:0] + {{(OWIDTH){1'b0}}, 1'b1, {(IWIDTH-OWIDTH-1){1'b0}}};
             assign out = hu[IWIDTH-1:IWIDTH-OWIDTH];
+        end else if(ROUND_TYPE == "HALF_DOWN") begin: half_down
+            wire [IWIDTH-1:0] hd = in[IWIDTH-1:0] + {{(OWIDTH){1'b0}}, 1'b0, {(IWIDTH-OWIDTH-1){1'b1}}};
+            assign out = hd[IWIDTH-1:IWIDTH-OWIDTH];
         end else if(ROUND_TYPE == "TO_ZERO") begin: to_zero
             wire [IWIDTH-1:0] tz = in[IWIDTH-1:0] + {{(OWIDTH){1'b0}}, in[IWIDTH-1], {(IWIDTH-OWIDTH-1){~in[IWIDTH-1]}}};
             assign out = tz[IWIDTH-1:IWIDTH-OWIDTH];
